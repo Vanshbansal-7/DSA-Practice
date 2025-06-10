1294f2038f7b0042238b2f666416ccaed285d183
Vansh Bansal <your-email@example.com>
2025-06-11 00:19:54 +0530

    PROGRAM FOR CHECKING PRIME NUMBER

--- BASIC JAVA/CHECKPRIME.java
+++ BASIC JAVA/CHECKPRIME.java
@@ -5,22 +5,21 @@ public class CHECKPRIME {
         Scanner sc = new Scanner(System.in);
         System.out.println("ENTER THE NUMBER");
         int num = sc.nextInt();
+        boolean flag=true;
         
         int div = 2;
         while (num > div) {
             if (num % div == 0) {
                 System.out.println("NUM ENTERED IS NOT PRIME");
+                flag=false;
                 break;
             }
             else {
                 div += 1;
             }
+        }
+        if (flag==true){
             System.out.println("YOU ENTERED PRIME NUMBER");
-            
-        
-        
-
-        
+        }
     }
-
 }
